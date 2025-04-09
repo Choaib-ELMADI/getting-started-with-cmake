c33b8698e3b31618c78ce8ed1cc8b77208d4a742
Choaib ELMADI <choaibamd@gmail.com>
2025-04-09 20:55:58 +0100

    Quickstart with CMake, Makefiles and Make

---
+++ build/CMakeFiles/my_project_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_project_test.dir/main.cpp.obj"
+  "CMakeFiles/my_project_test.dir/main.cpp.obj.d"
+  "libmy_project_test.dll.a"
+  "my_project_test.exe"
+  "my_project_test.exe.manifest"
+  "my_project_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_project_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
